4f02a4a4d2a892c792b1e0a7aeef441e4011ce20
张立聪 <zlc@qq.com>
2025-04-08 15:34:56 +0800

    feat: 增加性能测试

---
+++ ConsoleApp1/ExportEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class ExportEntity
+    {
+        [Qin.CsvRelevant.ExportColumn("Myorderid")]
+        public int Orderid { get; set; }
+
+        [Qin.CsvRelevant.ExportColumn("myname")]
+        public string Name { get; set; }
+
+        public string Name2 { get; set; }
+        public string Name3 { get; set; }
+        public string Area { get; set; }
+
+        [Qin.CsvRelevant.ExportColumn("myState")]
+        public int State { get; set; }
+        public byte Isdel { get; set; }
+        public decimal Money { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime? OtherTime { get; set; }
+    }
+}
